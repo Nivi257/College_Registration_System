9d5ed131d69e90560fb5c1a6433fea935ec14ddf
Nivi257 <157615831+Nivi257@users.noreply.github.com>
2024-06-27 09:28:50 +0530

    Add files via upload

---
+++ College_Registration_Assignment/src/main/java/com/example/college/CollegeRegistrationAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeRegistrationAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeRegistrationAssignmentApplication.class, args);
+	}
+
+}
